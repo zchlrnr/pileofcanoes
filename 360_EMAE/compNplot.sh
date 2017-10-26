bf6b28bfbe4710b6b34281f3f8188557545af145
Zach <zchlrnr@gmail.com>
2017-10-25 22:21:29 -0400

    Wonderful message about what I did

--- 360_EMAE/compNplot.sh
+++ 360_EMAE/compNplot.sh
@@ -1,8 +1,8 @@
-gfortran CalculateCrank.f -o CalculateCrank
-./CalculateCrank
+gfortran CalculateCrank.f -o CalculateCrank.q
+./CalculateCrank.q
 gnuplot -e "set terminal png size 800,600; set output 'Displacement.PNG'; plot 's.out'"
 gnuplot -e "set terminal png size 800,600; set output 'Max.PNG'; plot 'v.out'"
-gfortran CalculateCam.f -o CalculateCam
-./CalculateCam
+gfortran CalculateCam.f -o CalculateCam.q
+./CalculateCam.q
 gnuplot -e "set polar; set terminal png size 800,600; set output 'IntakeCam.PNG';plot 'intcams.out'"
 
