873ccde5baabbfb05b9b5ee5f10d9bec904c3cdc
Buck Baskin <mobile.wbaskin@gmail.com>
2017-11-09 14:57:06 -0500

    Add a little whitespace for readability

--- yocad/PythonAttempt/pythonattempt01.py
+++ yocad/PythonAttempt/pythonattempt01.py
@@ -6,6 +6,7 @@ from matplotlib.path import Path
 import matplotlib.patches as patches
 from scipy.special import binom
 from Bezier import Bezier
+
 Bdata = np.loadtxt('BearingSeatCoords_C.txt')
 Sdata = np.genfromtxt('specs.txt', usecols = 0, delimiter = ',', dtype = None)
 Pdata = np.loadtxt('profile.txt',delimiter = ',')
@@ -16,17 +17,22 @@ x2=[]
 y2=[]
 x3=[]
 y3=[]
+
 for i in range(0,len(Bdata)):
     x1=np.append(x1,Bdata[i,0])
     y1=np.append(y1,Bdata[i,1])
+
 for i in range(0,len(Pdata)):
     x2=np.append(x2,Pdata[i,0])
     y2=np.append(y2,Pdata[i,1])
+
 for i in range(0,len(Rdata)):
     x3=np.append(x3,Rdata[i,0])
     y3=np.append(y3,Rdata[i,1])
+
 xp, yp = Bezier(list(zip(x2,y2))).T
 xr, yr = Bezier(list(zip(x3,y3))).T
+
 plt.plot(xp,yp)
 plt.plot(xr,yr)
 plt.plot(x1,y1)
